8f75cfe8f003d07a311059fd994951371ae627ae
Daniel Briggs <dtbriggs123@gmail.com>
2024-04-14 10:58:38 -0700

    Revert "Merge branch 'main' into Daniel"
    This reverts commit 62ef801d9a5d8bcd93034ca687f7629706552d61, reversing
    changes made to e87c2f680db1b4dc88815de847f123e4feb8c169.

--- Spaced Out/Assets/CameraSwitcher.cs
+++ Spaced Out/Assets/CameraSwitcher.cs
@@ -13,7 +13,6 @@ public class CameraSwitcher : MonoBehaviour
     void Start()
     {
         mainCamera.gameObject.SetActive(true); 
-        showObjects(0);
     }
 
     void Update()
@@ -22,27 +21,8 @@ public class CameraSwitcher : MonoBehaviour
             setNeptuneScene();
          } else if (Input.GetKeyDown(KeyCode.R)) {
             setDefault();
-         } else if (Input.GetKeyDown(KeyCode.P)) {
-            setSaturn();
          }
     }
-
-    void showObjects(int index) {
-        for (int i = 0; i < SceneManager.sceneCount; i++) {
-            if (i == index) {
-                GameObject[] objectsInScene = SceneManager.GetSceneAt(i).GetRootGameObjects();
-                foreach (GameObject obj in objectsInScene) {
-                    obj.SetActive(true);
-                }
-            } else {
-                GameObject[] objectsInScene = SceneManager.GetSceneAt(i).GetRootGameObjects();
-                foreach (GameObject obj in objectsInScene) {
-                    obj.SetActive(false);
-                }
-            }
-        }
-    }
-
 
     void setNeptuneScene() {
         //float[] values = new float[] { 17.29f, 30f, 8.99f };
@@ -51,22 +31,15 @@ public class CameraSwitcher : MonoBehaviour
         //mainCamera.transform.position = new Vector3(values[0], values[1], values[2]);
         //mainCamera.transform.rotation = Quaternion.Euler(rotationValues[0], rotationValues[1], rotationValues[2]);
 
-        showObjects(1);
         SceneManager.LoadScene(1);
     }
 
     void setDefault() {
-        showObjects(0);
         SceneManager.LoadScene(0);
         //float[] values = new float[] { 4.566703f, 9.765378f, -4.990725f };
         //float[] rotationValues = new float[] { 0f, 0f, 0f };
 
         //mainCamera.transform.position = new Vector3(values[0], values[1], values[2]);
         //mainCamera.transform.rotation = Quaternion.Euler(rotationValues[0], rotationValues[1], rotationValues[2]);
-    }
-
-    void setSaturn() {
-        showObjects(2);
-        SceneManager.LoadScene(2);
     }
 }
